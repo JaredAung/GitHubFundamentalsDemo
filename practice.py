ded757aab5bc100b1b83ecf8aecb39ddd055c6ee
JaredAung <133064427+JaredAung@users.noreply.github.com>
2026-09-22 10:38:32 -0700

    update

---
+++ practice.py
@@ -0,0 +1,26 @@
+## Fundamentals
+# git add
+# git commit -m "commit message"
+# git push
+# git pull
+
+# Pull Requests
+# git branch 
+# git checkout -b branch_name / git switch -c branch_name
+# git checkout branch_name / git switch branch_name
+
+# Resets (Local Commit History: modify local commit history)
+# git reset --soft HEAD~1  : undo last commit, keep changes staged
+# git reset --mixed HEAD~1 : undo last commit, keep changes unstaged (default)
+# git reset --hard HEAD~1  : undo last commit and discard the changes
+
+# Reverts (Remote Commit: create a new commit to undo the last commit)
+# git revert HEAD : undo last commit, create a new commit
+# git push origin branch_name
+
+#Merge
+# git checkout main
+# git pull origin main
+# git merge branch_name
+
+
